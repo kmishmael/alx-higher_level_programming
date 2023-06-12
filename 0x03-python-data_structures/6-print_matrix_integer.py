519003be1e4f00e0e342dbe44192c5f7872404cb
kmishmael <kmishmael@gmail.com>
2023-06-12 21:27:48 +0300

    list of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end = '' if row.index(col) == len(row) - 1 else ' ')
+        print()
