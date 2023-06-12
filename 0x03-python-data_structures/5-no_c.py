e45461844522015151baff57dc8504d5c7dd3c9f
kmishmael <kmishmael@gmail.com>
2023-06-12 21:19:15 +0300

    I can C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for chr in my_string:
+        if chr == 'c' or chr == 'C':
+            continue
+        new_string += chr
+    return new_string
