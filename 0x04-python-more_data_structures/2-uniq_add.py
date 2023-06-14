68388a49b6974e059a2a77588d81b6d11fc99ea4
kmishmael <kmishmael@gmail.com>
2023-06-14 13:25:00 +0300

    unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    for idx, el in enumerate(my_list):
+        if el in my_list[:idx]:
+            continue
+        sum += el
+    return sum
