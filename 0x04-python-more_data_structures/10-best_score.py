610af1e721d7d47b4a2333f7c2c690d194d49021
kmishmael <kmishmael@gmail.com>
2023-06-14 14:53:15 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    key = None
+    if a_dictionary:
+        val = -9999999
+        for k, v in a_dictionary.items():
+            if val < v:
+                val = v
+                key = k
+    return key
