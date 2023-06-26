15259f1c143a30066d45f44538257ae22b3052a0
kmishmael <kmishmael@gmail.com>
2023-06-26 23:39:07 +0300

    integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def safe_print_division(a, b):
+    res = None
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
