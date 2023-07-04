90f27eaeef5622238da8de6bf76edd241a46e256
kmishmael <kmishmael@gmail.com>
2023-07-04 17:04:43 +0300

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+
+"Represents a sqaure"
+
+
+class Square:
+    """The square"""
+    def __init__(self, size=0):
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) == int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
