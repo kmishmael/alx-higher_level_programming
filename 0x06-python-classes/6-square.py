b56cf1b84e908ce5c92267dcebfb79a604c2737d
kmishmael <kmishmael@gmail.com>
2023-07-04 17:27:58 +0300

    coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+
+"Represents a sqaure"
+
+
+class Square:
+    """The square"""
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if type(position[0]) == int and type(position[1]) == int:
+            self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) == int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for j in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value[0]) == int and type(value[1]) == int:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
