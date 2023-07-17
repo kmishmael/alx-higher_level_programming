d6def4a265a5fd0659cac492dd95459a7573af23
kmishmael <kmishmael@gmail.com>
2023-07-17 16:03:51 +0300

    my int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,7 +9,7 @@ class MyInt(int):
     def __eq__(self, value):
         """override == operator"""
         return self.real != value
-    
+
     def __ne__(self, value):
         """override != operator"""
         return self.real == value
