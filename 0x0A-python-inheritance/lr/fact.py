efcc5fc8668d6d97b2ed2f6896710d5c9c0b52af
kmishmael <kmishmael@gmail.com>
2023-07-17 14:46:38 +0300

    add tests

---
+++ 0x0A-python-inheritance/lr/fact.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This is the "example" module
+>>> factorial(5)
+120
+"""
+
+def factorial(n):
+    """Return the factorial of n, an exact integer >= 0.
+
+    >>> [factorial(n) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+    """
+    import math
+    if not n >= 0:
+        raise ValueError("n must be >= 0")
+    if math.floor(n) != n:
+        raise ValueError("n must be exact integer")
+    if n+1 == n:  # catch a value like 1e300
+        raise OverflowError("n too large")
+    result = 1
+    factor = 2
+    while factor <= n:
+        result *= factor
+        factor += 1
+    return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
