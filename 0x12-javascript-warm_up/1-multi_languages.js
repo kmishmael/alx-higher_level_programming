6c4f1f059426f4908ed7efdb0d0310de790bef0b
kmishmael <kmishmael@gmail.com>
2023-09-12 02:19:20 +0300

    fix line print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const s = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(s);
