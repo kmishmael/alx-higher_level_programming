8d50754af37a14243c828e98187ecd5c33d2e5f7
kmishmael <kmishmael@gmail.com>
2023-09-12 02:07:51 +0300

    warm up js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+if (typeof parseInt(process.argv[2]) !== 'number' || isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(process.argv[2])}`);
+}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const word of words) {
+  console.log(word);
+}
